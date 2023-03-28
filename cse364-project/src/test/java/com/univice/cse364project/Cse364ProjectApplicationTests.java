e0cc1dad4298f68179c620890eb68c86d298a7b3
yongmandooo <kyle000131@gmail.com>
2023-03-28 22:34:20 +0900

    set spring project again

---
+++ cse364-project/src/test/java/com/univice/cse364project/Cse364ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univice.cse364project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cse364ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
