a1da1beecfe7f2f987acc313b53a530974ad194e
yongmandooo <kyle000131@gmail.com>
2023-04-04 22:10:00 +0900

    implement required APIs

--- cse364-project/src/main/java/com/univice/cse364project/movie/MovieController.java
+++ cse364-project/src/main/java/com/univice/cse364project/movie/MovieController.java
@@ -1,9 +1,12 @@
 package com.univice.cse364project.movie;
 
+import com.univice.cse364project.rating.Rating;
+import com.univice.cse364project.rating.RatingRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -13,9 +16,13 @@ public class MovieController {
     private final Logger LOG = LoggerFactory.getLogger(getClass());
     private final MovieRepository MovieRepository;
 
-    public MovieController(MovieRepository movieRepository) {
+    private final RatingRepository ratingRepository;
+
+    public MovieController(MovieRepository movieRepository, RatingRepository ratingRepository) {
         this.MovieRepository = movieRepository;
+        this.ratingRepository = ratingRepository;
     }
+    public  List<Rating> getRatings() {return ratingRepository.findAll();}
 
     @RequestMapping(value = "/movies", method = RequestMethod.GET)
     public List<Movie> getAllMovies(){
@@ -26,9 +33,30 @@ public class MovieController {
         return MovieRepository.save(movie);
     }
 
-//    @RequestMapping(value = "/ratings/{ratingValue}", method = RequestMethod.GET)
-//    public List<Movie> getOverRatingMovies(@PathVariable int ratingValue){
-//
-//    }
+    @RequestMapping(value = "/ratings/{ratingValue}", method = RequestMethod.GET)
+    public List<Movie> getOverRatingMovies(@PathVariable int ratingValue){
+        List<Movie> total = getAllMovies();
+        List<Movie> target = new ArrayList<>();
+        List<Rating> ratings = getRatings();
+
+        for(int i = 0; i < total.size(); i++ ){
+            Movie m = total.get(i);
+            long movieId = m.getMovieId();
+            int sum = 0;
+            int counter = 0;
+            for(int j = 0; j < ratings.size(); j++ ){
+                Rating r = ratings.get(j);
+                if(movieId == r.getMovieId()){
+                    sum += r.getRatingValue();
+                    counter++;
+                }
+            }
+            double avg = sum/counter;
+            if(avg >= ratingValue){
+                target.add(m);
+            }
+        }
+        return target;
+    }
 
 }
--- cse364-project/src/main/java/com/univice/cse364project/movie/MovieDALImpl.java
+++ cse364-project/src/main/java/com/univice/cse364project/movie/MovieDALImpl.java
@@ -1,5 +1,6 @@
 package com.univice.cse364project.movie;
 
+import com.univice.cse364project.rating.Rating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Repository;
@@ -11,6 +12,8 @@ public class MovieDALImpl implements MovieDAL {
     @Autowired
     private MongoTemplate mongoTemplate;
 
+    @Override
+    public List<Rating> getRatings() {return mongoTemplate.findAll(Rating.class);}
     @Override
     public List<Movie> getAllMovies() { return mongoTemplate.findAll(Movie.class);}
 
