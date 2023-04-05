8872cf9b596748daa7cb94dd109e648953551ece
uni20191131 <74632051+uni20191131@users.noreply.github.com>
2023-04-05 13:28:51 +0900

    Update Cse364ProjectApplicationTests.java

--- cse364-project/src/test/java/com/univice/cse364project/Cse364ProjectApplicationTests.java
+++ cse364-project/src/test/java/com/univice/cse364project/Cse364ProjectApplicationTests.java
@@ -2,7 +2,7 @@ package com.univice.cse364project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+@WebAppConfiguration
 @SpringBootTest
 class Cse364ProjectApplicationTests {
 
