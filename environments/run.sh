13fdff392d27f4ff394768e6d07a9dc63fbf4ef0
uni20191131 <74632051+uni20191131@users.noreply.github.com>
2023-04-05 19:16:15 +0900

    Update run.sh

--- environments/run.sh
+++ environments/run.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
+apt-get install sudo
+apt-get install systemctl
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+
+sudo apt-get install gnupg
+
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+
+sudo apt-get update
+sudo apt-get install libssl1.1
+echo "deb http://security.ubuntu.com/ubuntu focal-security main" | sudo tee /etc/apt/sources.list.d/focal-security.list
+sudo apt-get install libssl1.1
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+systemctl start mongod
 
 git clone https://github.com/yongmandooo/23Spring_SoftwareEngineering_Group5.git
 cd 23Spring_SoftwareEngineering_Group5/cse364-project
 mvn package
 java -jar target/cse364-project-0.0.1-SNAPSHOT.jar
+
+ 
