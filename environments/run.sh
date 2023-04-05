d90392c38e3d049c6e2cb296bf2aef8917bb5079
yongmandooo <kyle000131@gmail.com>
2023-04-05 23:42:21 +0900

    edit run.sh

--- environments/run.sh
+++ environments/run.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
-sudo apt-get install gnupg
+apt-get install -y gnupg
 
-wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
 
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 
-sudo apt-get update
-sudo apt-get install libssl1.1
+apt-get update
+apt-get install libssl1.1
 echo "deb http://security.ubuntu.com/ubuntu focal-security main" | sudo tee /etc/apt/sources.list.d/focal-security.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
+apt-get update
+apt-get install -y mongodb-org
 systemctl start mongod
 
 git clone https://github.com/yongmandooo/23Spring_SoftwareEngineering_Group5.git
 cd 23Spring_SoftwareEngineering_Group5/cse364-project
 mvn package
 java -jar target/cse364-project-0.0.1-SNAPSHOT.jar
-
- 
