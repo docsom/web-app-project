95378941d65beee032418589df1d62748554252c
kts0921 <kts0921@unist.ac.kr>
2023-05-01 23:40:55 +0900

    change InquiryRepository

--- cse364-project/src/main/java/com/univice/cse364project/inquiry/InquiryRepository.java
+++ cse364-project/src/main/java/com/univice/cse364project/inquiry/InquiryRepository.java
@@ -1,6 +1,8 @@
 package com.univice.cse364project.inquiry;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface InquiryRepository extends MongoRepository<Inquiry, String> {
 }
