d41019c65c87e246802d3e5c778b8d2cea94aa43
chanwoo <cks1091@unist.ac.kr>
2023-06-15 23:57:22 +0900

    war make

--- cse364-project/src/main/java/com/univice/cse364project/Cse364ProjectApplication.java
+++ cse364-project/src/main/java/com/univice/cse364project/Cse364ProjectApplication.java
@@ -3,11 +3,19 @@ package com.univice.cse364project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 @SpringBootApplication
-public class Cse364ProjectApplication {
+public class Cse364ProjectApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Cse364ProjectApplication.class, args);
+	}
+
+	// 이 부분 추가
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Cse364ProjectApplication.class);
 	}
 
 }
