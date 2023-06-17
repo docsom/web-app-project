72e8792577b5b3e8e87fa171eb88b2902c23c42f
chanwoo <cks1091@unist.ac.kr>
2023-06-18 01:20:14 +0900

    change port

--- cse364-project/src/main/java/com/univice/cse364project/Cse364ProjectApplication.java
+++ cse364-project/src/main/java/com/univice/cse364project/Cse364ProjectApplication.java
@@ -6,12 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 @SpringBootApplication
-public class Cse364ProjectApplication {
+public class Cse364ProjectApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Cse364ProjectApplication.class, args);
 	}
 
 	// 이 부분 추가
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Cse364ProjectApplication.class);
+	}
 
 }
