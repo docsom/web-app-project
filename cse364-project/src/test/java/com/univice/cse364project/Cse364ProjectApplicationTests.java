3829dd244230cc07b5f6fcb4553452109b24d058
chanwoo <cks1091@unist.ac.kr>
2023-06-17 19:49:34 +0900

    add servlet

--- cse364-project/src/test/java/com/univice/cse364project/Cse364ProjectApplicationTests.java
+++ cse364-project/src/test/java/com/univice/cse364project/Cse364ProjectApplicationTests.java
@@ -1,13 +1,23 @@
 package com.univice.cse364project;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootTest
-class Cse364ProjectApplicationTests {
+class Cse364ProjectApplicationTests extends SpringBootServletInitializer {
 
 	@Test
 	void contextLoads() {
 	}
+	public static void main(String[] args) {
+		SpringApplication.run(Cse364ProjectApplication.class, args);
+	}
 
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(new Class[]{Cse364ProjectApplication.class});
+	}
 }
