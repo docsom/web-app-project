e1bce1a8c83cde956b210901ac9147e2877d0616
태승 <52532069+docsom@users.noreply.github.com>
2023-06-20 23:29:58 +0900

    update environment

--- environments/run.sh
+++ environments/run.sh
@@ -13,6 +13,7 @@ apt-get install -y mongodb-org
 systemctl start mongod
 
 git clone https://github.com/yongmandooo/23Spring_SoftwareEngineering_Group5.git
+git checkout milestone3
 cd 23Spring_SoftwareEngineering_Group5/cse364-project
 mvn package
 java -jar target/cse364-project-0.0.1-SNAPSHOT.jar
